79d43677cd07ce830932a4d3aa105cf1a5329464
shruthik shetty <shrutikshetty14@gmail.com>
2024-12-29 21:05:05 +0530

    set up database connection

--- index.mjs
+++ index.mjs
@@ -1,11 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 // setting up express app
 const app = express();
 
 //configring the dotenv to read env variables
 dotenv.config();
+
+//connect to the database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("successfully connected to the database");
+  })
+  .catch((err) => {
+    console.log("failed to connect to the data base ");
+  });
 
 //routes
 app.get("/", (req, res) => {
